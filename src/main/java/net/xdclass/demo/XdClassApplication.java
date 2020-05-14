f4b4216f3d30f02fba40ec6c46bea29a44804b44
HDZ <hdz42@outlook.com>
2020-05-15 01:08:19 +0800

    无用代码删除

--- src/main/java/net/xdclass/demo/XdClassApplication.java
+++ src/main/java/net/xdclass/demo/XdClassApplication.java
@@ -17,20 +17,5 @@ public class XdClassApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(XdClassApplication.class, args);
 	}
-/*
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}*/
 
 }
