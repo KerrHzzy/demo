908658ea2f5b7d0cadfbdfbd4f1f6eb0ceb3a7a9
Huang DiaoZhong <VPHT204@globalymc.com>
2020-05-15 14:13:13 +0800

    jar包方式运行web项目的文件上传和访问处理（核心知识）

--- src/main/java/net/xdclass/demo/XdClassApplication.java
+++ src/main/java/net/xdclass/demo/XdClassApplication.java
@@ -1,12 +1,12 @@
 package net.xdclass.demo;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * @author hdz
@@ -18,4 +18,13 @@ public class XdClassApplication {
 		SpringApplication.run(XdClassApplication.class, args);
 	}
 
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//单个文件最大KB,MB
+		factory.setMaxFileSize(DataSize.parse("10240KB"));
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize(DataSize.parse("1024000KB"));
+		return factory.createMultipartConfig();
+	}
 }
